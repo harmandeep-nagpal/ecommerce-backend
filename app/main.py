0704deaa5586cfa210beac4f4ea27ad66ef9a9b8
Harmandeep Nagpal <harmandeep01633@gmail.com>
2026-07-03 16:46:28 +0530

    Day 3: Added validation, error handling and response models

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
-from fastapi import FastAPI
-from app.schemas import Product
+from fastapi import FastAPI, HTTPException
+from app.schemas import Product, ProductResponse
 
 APP_NAME = "E-commerce Backend"
 
@@ -8,6 +8,7 @@ APP_DESCRIPTION = "Production-ready e-commerce backend built with FastAPI."
 APP_VERSION = "1.0.0"
 
 products = []
+next_product_id = 1
 
 app = FastAPI(
     title=APP_NAME,
@@ -35,10 +36,18 @@ def get_version():
 
 @app.post("/products")
 def create_product(product: Product):
-    products.append(product)
+    global next_product_id
+
+    product_dict = product.model_dump()      # Convert Pydantic model to dictionary
+    product_dict["id"] = next_product_id     # Assign ID
+
+    products.append(product_dict)
+
+    next_product_id += 1                     # Increment for next product
+
     return {
         "message": "Product created successfully",
-        "product": product
+        "product": product_dict
     }
 
 @app.get("/products")
@@ -49,14 +58,37 @@ def get_products(limit: int = 10, skip: int = 0):
         "limit": limit,
         "skip": skip
         }
-
+        
+@app.get("/products/{product_id}",
+        response_model=ProductResponse) # It tells FastAPI: "Whatever this function returns must match the ProductResponse schema."
+def get_product_by_id(product_id: int):
+    for product in products:
+        if product["id"] == product_id:
+            return {
+                "product": product,
+                "message": "Product found"
+            }
+    raise HTTPException(
+            status_code=404,
+            detail=f"Product with ID {product_id} not found"
+        )
+            
 
 @app.put("/products/{product_id}")
-def update_product(product_id: int):
-    return {
-        "product_id": product_id,
-        "message": "Product updated"
-    }
+def update_product(product_id: int, updated_product: Product):
+    for product in products:
+        if product["id"] == product_id:
+            product["name"] = updated_product.name
+            product["price"] = updated_product.price
+            product["stock"] = updated_product.stock
+            return {
+                "product" : product,
+                "message" : f"The updated producct with the product id {product_id} is"
+            } 
+    raise HTTPException(
+        status_code=404,
+        detail=f"Product with ID {product_id} not found"
+    )
 
 
 @app.patch("/products/{product_id}")
@@ -69,7 +101,14 @@ def patch_product(product_id: int):
 
 @app.delete("/products/{product_id}")
 def delete_product(product_id: int):
-    return {
-        "product_id": product_id,
-        "message": "Product deleted"
-    }
+    for product in products:
+        if product["id"] == product_id:
+            products.remove(product)
+            return {
+                "product_id": product_id,
+                "message": "Product deleted"
+            }
+    raise HTTPException(
+        status_code=404,
+        detail= f"The product with ID = {product_id} can't be found"
+    )
--- app/schemas.py
+++ app/schemas.py
@@ -1,6 +1,28 @@
-from pydantic import BaseModel 
+from pydantic import BaseModel, Field 
 
 class Product(BaseModel): # BaseModel is the class provided by Pydantic that enables automatic validation and parsing.
-    name: str
-    price: float
-    stock: int
+    name: str = Field(
+        min_length=3,
+        max_length=100, 
+        description="The name of the product")
+    price: float = Field(
+        gt=0, 
+        description="The price of the product")
+    stock: int = Field(
+        ge=0,
+        description = "Available units in the inventory")
+    
+class ProductResponse(BaseModel):  # Response model
+    id : int = Field(
+        gt=0
+    )
+    name: str = Field(
+        min_length=3,
+        max_length=100, 
+        description="The name of the product")
+    price: float = Field(
+        gt=0, 
+        description="The price of the product")
+    stock: int = Field(
+        ge=0,
+        description = "Available units in the inventory")
